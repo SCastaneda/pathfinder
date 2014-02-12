66a0b1d3b9554bcd935814191635e7e0f3e2a9a7
Samuel Castaneda <sammy.castaneda90@gmail.com>
2014-02-11 21:52:52 -0500

    set up the waiting room, ready for mark to start

--- waiting.js
+++ waiting.js
@@ -1,15 +1,14 @@
 function User(socket, name) {
     this.socket = socket;
-    this.name = name;
+    this.name   = name;
 }
 var all_users = [];
 
 exports.start = function(server, cookieParser, sessionStore) {
     var io = require("socket.io").listen(server);
 
-    var SessionSockets = require('session.socket.io')
-      , sessionSockets = new SessionSockets(io, sessionStore, cookieParser);
-
+    var SessionSockets = require('session.socket.io');
+    var sessionSockets = new SessionSockets(io, sessionStore, cookieParser);
 
     // this event gets called when a user connects
     sessionSockets.on('connection', connect);
@@ -18,84 +17,89 @@ exports.start = function(server, cookieParser, sessionStore) {
     function connect(err, socket, session) {
 
         // only let users in that have a session
-        if(session) {
+        if(session.name) {
             var user = new User(socket, session.name);
-
-            // need to check if the user is already here, before we push
-            get_user(socket, function(user) {
-                if(user) {
-                    console.log(user.name + " connected");
-                    all_users.push(user);
-                    console.log(all_users);
-                } else {
-                    console.log("already have sockets, not added");
-                    console.log(all_users);
-                }
-            });
+            add_user(socket, user);
         } else {
             // redirects the user back to the home page /
             socket.emit('disconnect', {});
         }
 
-        // access user name through user.name
-        
-
-        // all users initially join the waiting room
-        socket.join('waiting');
-        io.sockets.in('waiting').emit('update_total', { count: all_users.length });
-
-        if(all_users.length > 1) {
-            // grab 2 users, make a unique hash for them, 
-            // and redirect them to their game room
-
-        }
-
-        socket.on('disconnect', disconnect);
+        socket.on('disconnect', function() {
+            disconnect(socket);
+        });
     }
 
-    function disconnect(err, socket) {
-        
+    function disconnect(socket) {
         get_user(socket, function(user) {
             if(user) {
                 console.log(user.name + " disconnected");
             }
         });
         
-        delete_user(socket, function(all_users) {
-            if(all_users) {
+        delete_user(socket, function(deleted) {
+            if(deleted) {
                 io.sockets.in('waiting').emit('update_total', { count: all_users.length });
             } else {
                 console.log("can't update total: " + all_users);
             }
         });
-
     }
-}
 
-// gets the user from the all_users array of 'Users'
-function get_user(socket, cb) {
-    for(var i = 0; i < all_users.length; i++) {
-        if(all_users[i].socket == socket) {
-            cb(all_users[i]);
-        }
-        if(i == all_users.length-1) {
+    // gets the user from the all_users array of 'Users'
+    function get_user(socket, cb) {
+
+        sessionSockets.getSession(socket, function(err, session) {
+            for(var i = 0; i < all_users.length; i++) {
+                if(all_users[i].name == session.name) {
+                    cb(all_users[i]);
+                }
+                
+            }
+            console.log(session.name + ": " + all_users);
             cb(false);
-        }
+        });
     }
 
-}
+    function delete_user(socket, cb) {
 
-function delete_user(socket, cb) {
-    // find and remove user from all users list
-    for(var i = 0; i < all_users.length; i++) {
-        if(all_users[i].socket == socket) {
-            all_users.splice(i, 1);
-            cb(all_users);
-        }
-        if(i == all_users.length-1) {
+        sessionSockets.getSession(socket, function(err, session) {
+            // find and remove user from all users list
+            for(var i = 0; i < all_users.length; i++) {
+                if(all_users[i].name == session.name) {
+                    all_users.splice(i, 1);
+                    cb(true);
+                }
+            
+            }
+            console.log(session.name + ": " + all_users);
             cb(false);
-        }
-
+        });
     }
-    // cb(false);
+
+    function add_user(socket, user) {
+        // need to check if the user is already here, before we push
+        get_user(socket, function(user_already_here) {
+            if(!user_already_here) {
+                console.log(user.name + " connected");
+                all_users.push(user);
+                console.log(all_users);
+
+                // all users initially join the waiting room
+                socket.join('waiting');
+                io.sockets.in('waiting').emit('update_total', { count: all_users.length });
+
+                if(all_users.length > 1) {
+                    // grab 2 users, make a unique hash for them, 
+                    // and redirect them to their game room
+
+                }
+            } else {
+                console.log("already have socket, not added");
+                console.log(all_users);
+            }
+        });
+    }
 }
+
+
